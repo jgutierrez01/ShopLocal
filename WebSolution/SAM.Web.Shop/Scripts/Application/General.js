70dd1fee46e02fa67dc62cdc12fdc1c9150a71fe
FabianGtz <jgutierrez@anbintec.com.mx>
2018-01-04 09:42:21 -0600

    Se agrega estilos al grid kendo y se agrega splash de loading

---
+++ WebSolution/SAM.Web.Shop/Scripts/Application/General.js
@@ -0,0 +1,19 @@
+﻿//Function to activate waiting screen
+function loadingStart() {
+    $(document).on('keypress keydown keyup', function (e) {
+        if (e.which === 8 || e.which === 38 || e.which === 40) {
+            e.stopImmediatePropagation();
+            return false;
+        };
+        return false;
+    });
+    $("body").attr("style", "pointer-events:none;");
+    $("body").addClass("loading");
+}
+
+//Function to deactivate waiting screen
+function loadingStop() {
+    $("body").removeAttr("style", "pointer-events:none;");
+    $("body").removeClass("loading");
+    $(document).off('keypress keydown keyup');
+}
