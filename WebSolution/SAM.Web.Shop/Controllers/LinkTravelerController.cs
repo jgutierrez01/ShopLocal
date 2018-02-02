3432a09789a110bf394af5f57ca872779cd399b4
Fabian Gutierrez <jgutierrez@anbintec.com.mx>
2018-02-01 23:54:28 -0600

    Validacion de numero de paginas negativos

--- WebSolution/SAM.Web.Shop/Controllers/LinkTravelerController.cs
+++ WebSolution/SAM.Web.Shop/Controllers/LinkTravelerController.cs
@@ -37,35 +37,44 @@ namespace SAM.Web.Shop.Controllers
                 {                    
                     //Existe el pdf en el servidor se agrega logica de negocio para retornar el traveler
                     string ruta = Path.Combine(Proyecto.pathPDF, "ODT " + dtSpools.Rows[0][0].ToString() + ".pdf");
-                    int numeroPaginasPDF = SplitPDF.Instance.CantidadDePaginas(ruta) - int.Parse(dtSpools.Rows[0][2].ToString());                              
-                    Document doc = new Document();
-                    string nombreTemporal = "\\ODT_" + NumeroControl + ".pdf";
-                    MemoryStream stream = new MemoryStream();
-                    Document document = new Document();
-                    PdfCopy writer = new PdfCopy(document, stream);
-                    writer.CloseStream = false;
-                    PdfImportedPage page = null;
-                    document.Open();                                   
-                    PdfReader reader = new PdfReader(ruta);                    
-                    page = writer.GetImportedPage(reader, numeroPaginasPDF);
-                    writer.AddPage(page);                        
-                    document.Close();
-                    reader.Close();
+                    int numeroPaginasPDF = SplitPDF.Instance.CantidadDePaginas(ruta) - int.Parse(dtSpools.Rows[0][2].ToString());
 
-                    Response.Clear();
-                    //Response.AddHeader("Content-Disposition", "attachment;filename=ODT_" + NumeroControl + ".pdf");                    
-                    Response.AddHeader("Pragma", "no-cache");
-                    //Response.ContentType = "application/pdf";                    
-                    Response.ContentType = "application/force-download";
-                    //Response.Buffer = true;
-                    //Response.OutputStream.Write(stream.GetBuffer(), 0, stream.GetBuffer().Length);
-                    //Response.OutputStream.Flush();
-                    //Response.Flush();
-                    //Response.Close();
-                    //Response.End();
-                    //return new FileStreamResult(Response.OutputStream, "application/pdf");                                       
-                    Response.BinaryWrite(stream.ToArray());
-                    return File(stream.ToArray(), "application/pdf");                                    
+                    if(numeroPaginasPDF > 0)
+                    {
+                        Document doc = new Document();
+                        string nombreTemporal = "\\ODT_" + NumeroControl + ".pdf";
+                        MemoryStream stream = new MemoryStream();
+                        Document document = new Document();
+                        PdfCopy writer = new PdfCopy(document, stream);
+                        writer.CloseStream = false;
+                        PdfImportedPage page = null;
+                        document.Open();
+                        PdfReader reader = new PdfReader(ruta);
+                        page = writer.GetImportedPage(reader, numeroPaginasPDF);
+                        writer.AddPage(page);
+                        document.Close();
+                        reader.Close();
+
+                        Response.Clear();
+                        //Response.AddHeader("Content-Disposition", "attachment;filename=ODT_" + NumeroControl + ".pdf");
+                        Response.AddHeader("Pragma", "no-cache");
+                        Response.ContentType = "application/force-download";
+                        Response.BinaryWrite(stream.ToArray());
+                        return File(stream.ToArray(), "application/pdf");
+                    }
+                    else
+                    {
+                        MemoryStream workStream = new MemoryStream();
+                        Document document = new Document();
+                        PdfWriter.GetInstance(document, workStream).CloseStream = false;
+                        document.Open();
+                        document.Add(new Paragraph("El Archivo PDF De Origen Está Incompleto Ó No Coincide Con El Número De Páginas."));
+                        document.Close();
+                        byte[] byteInfo = workStream.ToArray();
+                        workStream.Write(byteInfo, 0, byteInfo.Length);
+                        workStream.Position = 0;
+                        return new FileStreamResult(workStream, "application/pdf");
+                    }                                                        
                 }
                 else
                 {
